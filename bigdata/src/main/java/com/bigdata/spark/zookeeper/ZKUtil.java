086d124f5ce89ce8a1a37e8671894fa2bafff703
陈惜时 <116651445+YongqiChen0807@users.noreply.github.com>
2023-02-03 18:26:45 +0800

    Create ZKUtil.java

--- bigdata/src/main/java/com/bigdata/spark/zookeeper/ZKUtil.java
+++ bigdata/src/main/java/com/bigdata/spark/zookeeper/ZKUtil.java
@@ -1,9 +1,8 @@
 package com.bigdata.spark.zookeeper;
 /**
- * @author   杨俊
- * @contact  咨询微信:dashuju_2017
- * @created time 2022-04-10
- */
+ * @author   陈惜时
+ * @contact Zookeeper实现分布式爬虫监控
+ * @created time 2022-01-12 */
 public class ZKUtil {
     public static final String ZOOKEEPER_HOSTS = "192.168.26.161:2181";
     public static final String PATH = "/spider";
